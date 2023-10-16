25e92611ff495658100e6674b417211cf2b5c3d1
Diego Soares de Oliveira <ddigson@gmail.com>
2023-10-16 15:39:42 -0300

    criado microservico cupom de desconto

---
+++ GeekShopping/GeekShopping.CouponAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.CouponAPI.Model.Context
+{
+    public class MySQLContext : DbContext ///construtor
+    {        
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) {} //<<-- objeto do qual estende do dbcontext
+        public DbSet<Coupon> Coupons { get; set; } //adicionado de produtos
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                Id = 1,
+                CouponCode = "ERUDIO_2022_10",
+                DiscountAmount = 10
+            });
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                Id = 2,
+                CouponCode = "ERUDIO_2022_15",
+                DiscountAmount = 15
+            });
+        }
+
+
+
+
+    }
+}
